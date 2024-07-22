56f968b096a804becda947a37831dfe521bc6f3a
Sushil37 <sushil.shrestha3000@gmail.com>
2024-07-22 23:57:23 +0545

    chore: remove un-used code

--- index.js
+++
@@ -1,8 +0,0 @@
-const app = express();
-
-app.get('/users', (req, res) => {
-  connection.query('SELECT * FROM users', (err, results) => {
-    if (err) throw err;
-    res.json(results);
-  });
-});
